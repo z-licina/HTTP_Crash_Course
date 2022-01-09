4ef65133f12db71ecd239c661ed292ea8c4911ff
userName1 <hello@gmail.com>
2022-01-09 21:09:34 +0100

    Static folder

--- index.js
+++ index.js
@@ -5,10 +5,6 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-
-app.delete('/post/:id', (req, res) => {
-    // DATABASE STUFF
-    res.json({msg: `Post ${req.params.id} deleted`})
-})
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(5000, ()=> console.log(`Server started on 5000`));
