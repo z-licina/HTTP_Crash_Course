a2d5296476694103cb2971a6e6f518fa2682f1be
userName1 <hello@gmail.com>
2022-01-09 21:00:37 +0100

    PUT Request

--- index.js
+++ index.js
@@ -6,22 +6,13 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.post('/contact', (req, res) => {
-    if (!req.body.name){
-        return res.status(400).send("Name is required");
-    }
+// ':id' like placeholder
+app.put('/post/:id', (req, res) => {
     // DATABASE STUFF
-    res.status(201).send(`Thank you ${req.body.name}`)
-})
-
-app.post('/login', (req, res) => {
-    if (!req.header('x-auth-token')){
-        return res.status(400).send("No Token");
-    }
-    if (req.header('x-auth-token') === '123456'){
-        return res.status(401).send("Not authorized");
-    }
-    return res.send('Logged in');
+    res.json({
+        id: req.params.id,
+        title: "My title"
+    })
 })
 
 app.listen(5000, ()=> console.log(`Server started on 5000`));
